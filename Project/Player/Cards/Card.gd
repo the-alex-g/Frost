adf8a5e4ee4f61e80e3cedddaebc527bb43c0bd9
Alex Gestwicki <alex.gestwicki@gmail.com>
2020-06-04 20:28:38 -0400

    Expand deck

--- Project/Player/Cards/Card.gd
+++ Project/Player/Cards/Card.gd
@@ -5,9 +5,18 @@ var cardname:String
 var damage:int
 var health:int
 var cost:int
+var hovered:bool = false
 
 func _ready():
-	text.text += str(cost)+"\n \n"
+	text.text += cardname+" "+str(cost)+"\n \n"
 	text.text += str(damage)
 	if health != 0:
-		text.text += "\n"+str(health)
+		text.text += "|"+str(health)
+
+func _on_RichTextLabel_mouse_entered():
+	modulate = Color(1,1,0,1)
+	hovered = true
+
+func _on_RichTextLabel_mouse_exited():
+	modulate = Color(1,1,1,1)
+	hovered = false
--- Project/Player/Player.gd
+++ Project/Player/Player.gd
@@ -6,24 +6,26 @@ var decksize:int = -1
 var type
 var handspace:Vector2 = Vector2(0,0)
 var deck:Array = [
-	{"damage":2, "cost":1, "health":0, "number":3}, {"damage":1, "health":1, "cost":1, "number":3},
-	{"damage":1, "health":3, "cost":2, "number":2}
+	{"name":"Fire Blast", "damage":2, "cost":1, "health":0, "number":3}, {"name":"Flame Spirit", "damage":1, "health":1, "cost":1, "number":3},
+	{"name":"Flame Shield", "damage":0, "health":2, "cost":1, "number":3}, {"name":"Fire Crab", "damage":1, "health":3, "cost":2, "number":2},
+	{"name":"Drake", "damage":2, "health":2, "cost":2, "number":2}, {"name":"Fire Giant", "damage":3, "health":3, "cost":3, "number":1}
 ]
 
 func _ready():
-	drawcard(3)
+	drawcards(3)
 
 func findvar(variable:String):
 	var foundvar = deck[type][variable]
 	return foundvar
 
-func drawcard(size):
+func drawcards(number:int):
 	for item in deck:
 		decksize += 1
-	for x in range(0,size):
+	for _x in range(0,number):
 		randomize()
 		type = int(round(rand_range(0,decksize)))
 		var card = cards.instance()
+		card.cardname = findvar("name")
 		card.damage = findvar("damage")
 		card.health = findvar("health")
 		card.cost = findvar("cost")
@@ -33,5 +35,5 @@ func drawcard(size):
 		else:
 			deck[type]["number"] -= 1
 		card.position = handspace
-		handspace.x += 20
+		handspace.x += 100
 		hand.add_child(card)
